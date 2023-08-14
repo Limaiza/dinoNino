2f1e7052e097d6b86cc2f0caf2a9e0a4f6930ba5
Ilima <iza.dl@hotmail.com>
2023-08-14 15:52:26 -0300

    Tarefa Aula 2

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -3,7 +3,7 @@ import random
 from dino_runner.components.obstacles.obstacle import Obstacle
 
 
-class Bird (Obstacle):
+class Bird(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
