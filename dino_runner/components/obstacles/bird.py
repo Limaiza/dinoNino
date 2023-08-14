06010a188be6b8cd57019edaa585ece539f91c8f
Ilima <iza.dl@hotmail.com>
2023-08-14 19:07:41 -0300

    Tarefa Aula 2 Obstáculos add, menos a animação do passáro

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -7,4 +7,4 @@ class Bird(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = 250
+        self.rect.y = 240
