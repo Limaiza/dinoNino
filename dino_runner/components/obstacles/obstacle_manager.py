2ad1fdb6fc7878752b69c9c8530b2c30fe44ac33
Ilima <iza.dl@hotmail.com>
2023-08-14 15:32:29 -0300

    Tarefa Aula 2

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,8 +1,10 @@
 import pygame
 
 from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
 from dino_runner.utils.constants import SMALL_CACTUS
 from dino_runner.utils.constants import LARGE_CACTUS
+from dino_runner.utils.constants import BIRD
 
 
 class ObstacleManager:
@@ -13,6 +15,7 @@ class ObstacleManager:
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus(SMALL_CACTUS))
             self.obstacles.append(Cactus(LARGE_CACTUS))
+            self.obstacles.append(Bird(BIRD))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
